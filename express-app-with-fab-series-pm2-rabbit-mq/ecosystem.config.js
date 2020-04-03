82b8105846248a209ae394a8e7531d543f64439c
Andrii Zilnyk <andrii.zilnyk@wetelo.com>
2020-04-03 23:33:43 +0300

    first commit

---
+++ express-app-with-fab-series-pm2-rabbit-mq/ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Express App',
+      script: 'server.js',
+      instances: 2,
+      autorestart: true,
+      exec_mode: 'cluster',
+      watch: true,
+      max_memory_restart: '1G'
+    },
+    {
+      name: 'Worker1',
+      script: 'workers/fab-series-worker1.js',
+      instances: 1,
+      watch: true,
+    },
+    {
+      name: 'Worker2',
+      script: 'workers/fab-series-worker2.js',
+      instances: 1,
+      watch: true,
+    }
+  ]
+};
